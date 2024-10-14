10e3830cad7eb84b9453550162dd18d5b80356d2
perquis <thetahirpl@gmail.com>
2024-10-14 17:47:14 +0200

    :wrench: config(eslint): Updated the eslint configuration file.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginCypress from 'eslint-plugin-cypress/flat'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import pluginCypress from 'eslint-plugin-cypress/flat';
+import pluginVue from 'eslint-plugin-vue';
 
 export default [
   {
@@ -17,18 +17,18 @@ export default [
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
       'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}' // use single quotes as in the other configs
+      'cypress/support/**/*.{js,ts,jsx,tsx}', // use single quotes as in the other configs
     ],
   },
   skipFormatting,
-]
+];
