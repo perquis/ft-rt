a289f8d54b5df041fc6d1f1a0d56c5788755ee17
perquis <thetahirpl@gmail.com>
2024-10-14 20:34:28 +0200

    :lipstick: ui(PrimaryButton): Added animation to this component.

--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="flex flex-shrink-0 items-center gap-2 rounded-full bg-emerald-600 px-4 py-2 font-medium text-white"
+    class="flex flex-shrink-0 items-center gap-2 rounded-full bg-emerald-600 px-4 py-2 font-medium text-white duration-200 ease-in-out hover:opacity-90 focus-visible:opacity-90"
   >
     <slot></slot>
   </button>
